31981b91817b47dd014d110efb1681e7f85a53ac
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-07-18 10:53:52 -0400

    Refactor code

--- Day-12/main.py
+++ Day-12/main.py
@@ -11,31 +11,40 @@ def check_guess(guess, number):
     elif guess < number:
         print("You are too low! Guess again.")
     else:
-        print("You guessed correctly!")
+        print(f"You guessed correctly! The number was {number}!")
 
 
-system("clear")
-print(art.logo)
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-number = random.randint(1, 100)
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-
-attempts = 0
-if difficulty == "easy":
-    attempts = 10
-else:
-    attempts = 5
-
-
-while attempts > 0:
-    guess = int(input("Guess a number: "))
-    check_guess(guess, number)
-    if guess == number:
-        break
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    EASY_DIFF = 10
+    HARD_DIFF = 5
+    if difficulty == "easy":
+        return EASY_DIFF
     else:
-        attempts -= 1
-        print(f"You have {attempts} attempts left.")
+        return HARD_DIFF
 
-if attempts == 0:
-    print(f"The number was {number}.")
+
+def game():
+    """Plays the number game."""
+    system("clear")
+    print(art.logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    number = random.randint(1, 100)
+
+    attempts = set_difficulty()
+
+    while attempts > 0:
+        guess = int(input("Guess a number: "))
+        check_guess(guess, number)
+        if guess == number:
+            break
+        else:
+            attempts -= 1
+            print(f"You have {attempts} attempts left.")
+
+    if attempts == 0:
+        print(f"The number was {number}.")
+
+
+game()
