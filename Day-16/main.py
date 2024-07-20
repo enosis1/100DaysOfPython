3e7d5f07256dd569189f577e18de2170898447a8
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-07-19 23:29:44 -0400

    Finish code

--- Day-16/main.py
+++ Day-16/main.py
@@ -1,3 +1,27 @@
 from coffee_maker import CoffeeMaker
 from menu import Menu, MenuItem
 from money_machine import MoneyMachine
+
+coffee_machine = CoffeeMaker()
+menu = Menu()
+money = MoneyMachine()
+
+
+while True:
+    order = input(f"What would you like? ({menu.get_items()}): ")
+
+    if order == "report":
+        coffee_machine.report()
+        money.report()
+        continue
+    elif order == "off":
+        print("Shutting down...")
+        break
+    elif menu.find_drink(order) is None:
+        continue
+    else:
+        order = menu.find_drink(order)
+
+    if coffee_machine.is_resource_sufficient(order):
+        money.make_payment(order.cost)
+        coffee_machine.make_coffee(order)
