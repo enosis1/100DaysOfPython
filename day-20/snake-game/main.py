45ac858b49ee4de6a3756901701caec3fe0cfadc
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2024-07-24 10:58:06 -0400

    convert code into snake class

--- day-20/snake-game/main.py
+++ day-20/snake-game/main.py
@@ -1,8 +1,29 @@
+import time
 from turtle import Screen, Turtle
+from snake import Snake
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake")
+screen.tracer(0)
+
+# TODO: Create a snake body
+snake = Snake()
+# TODO: Move the snake
+
+screen.update()
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+
+# TODO: Create snake food
+# TODO: Detect collision with food
+# TODO: Create a scoreboard
+# TODO: Detect collision with wall
+# TODO: Detect collision with tail
+
 
 screen.exitonclick()
